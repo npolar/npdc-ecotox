07708f106485984bb6f89a1444dcef536a90259c
srldl <siri.uldal@gmail.com>
2019-02-12 15:54:40 +0100

    intermediate

--- src/ecotox-fieldwork/edit/EcotoxFieldworkEditController.js
+++ src/ecotox-fieldwork/edit/EcotoxFieldworkEditController.js
@@ -5,53 +5,127 @@ var EcotoxFieldworkEditController = function($http, $scope, $location, $controll
   NpolarApiSecurity, npolarCountryService, NpdcSearchService, NpolarMessage) {
   'ngInject';
 
-   $controller('NpolarEditController', { $scope: $scope });
-   $scope.resource = EcotoxTemplate;
+  var $  = require( 'jquery' );
+  var dt = require( 'datatables.net' )( window, $ );
+  require('datatables.net-select' )( window, $ );
+  var tb = require( '@srldl/edit-tabletest/js/edit-table.js');
 
-    let query = function(search_id,db_key) {
+  $controller('NpolarEditController', { $scope: $scope });
 
-        let defaults;
+  $scope.resource = EcotoxTemplate;
+  $scope.ecotoxTab = tb.tabRow;
 
-        if (db_key==="template"){
-          defaults = {
-            "filter-id": search_id
-          };
-          console.log(defaults);
-        } else {
-          defaults = {
-            "filter-ecotox_template": search_id
-          };
-          console.log(defaults);
-        }
+       console.log("hei");
 
-      let invariants = $scope.security.isAuthenticated() ? {} : {} ;
-      return Object.assign({}, defaults, invariants);
-      };
+     //Query for the database
+  /*   let query = function(search_id,db_key) {
+         let defaults;
 
+         if (db_key==="template"){
+           defaults = {
+             "filter-id": search_id
+           };
+         } else {
+           defaults = {
+             "filter-ecotox_template": search_id
+           };
+         }
 
-    var res_template = $scope.search(query("a11a7305-45a8-4ad2-80d9-60f4b8980cc3","template"));
-      console.log(res_template);
+       let invariants = $scope.security.isAuthenticated() ? {} : {} ;
+       return Object.assign({}, defaults, invariants);
+     }; */
 
-    $scope.resource = EcotoxFieldwork;
-    var res_fieldwork = $scope.search(query("a11a7305-45a8-4ad2-80d9-60f4b8980cc3","fieldwork"));
-    console.log(res_fieldwork);
+     //search for template
+     //var res_template = $scope.search(query("a11a7305-45a8-4ad2-80d9-60f4b8980cc3","template"));
+    // console.log(res_template);
 
+     //Search for fieldwork
+    // $scope.resource = EcotoxFieldwork;
+    // var res_fieldwork = $scope.search(query("a11a7305-45a8-4ad2-80d9-60f4b8980cc3","fieldwork"));
+    // console.log(res_fieldwork);
 
+    var dataSet = [
+               [ "1","Tiger Nixon", "System Architect", "Edinburgh", "5421", "2011/04/25", "$320,800" ],
+               [  "2","Garrett Winters", "Accountant", "Tokyo", "8422", "2011/07/25", "$170,750" ],
+               [  "3","Ashton Cox", "Junior Technical Author", "San Francisco", "1562", "2009/01/12", "$86,000" ]
+         ];
 
-  //Jump to another tab
-  $scope.ecotoxTab = function(label) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(label).style.display = "block";
-    //evt.currentTarget.className += " active";
-  };
+console.log("2");
+
+       $(document).ready(function() {
+         var table = $('#example').DataTable( {
+                 select: true,
+                 autofill: true,
+                 data: dataSet,
+                 columns: [
+                     { title: "ID" },
+                     { title: "Name" },
+                     { title: "Position" },
+                     { title: "Office" },
+                     { title: "Extn." },
+                     { title: "Start date" },
+                     { title: "Salary" }
+                 ]
+             } );
+
+        /*     $('#saveBtn').click( function() {
+               var data = table.$('input, select').serialize();
+                //console.log(data[3].value);  //.substr( 0, 120 ));
+                console.log( data);
+                 return false;
+             } );
+
+             var e1 = 'Eva 2 Olsen';
+             var e2 = '<td><input type="text" value=""></td>';
+             var e3 = 'Edinburgh';
+             var e4 = "8422";
+             var e5 = "2011/07/25";
+             var e6 = "$170,750";
+
+             $('#addBtn').click( function() {
+                 console.log( e1);
+                 var rowNode = table.row.add( [e1, e2, e3, e4, e5, e6] ).draw().node();
+
+                $( rowNode )
+                .css( 'color', 'red' )
+                .animate( { color: 'blue' } );
+
+                   console.log( table.data() );
+
+                 return false;
+             } );
+
+             $('#newBtn').click( function() {
+                var e1 = e3 = e4 = e5 = e6 = '<td><input type="text" name="fname"></td>';
+                var e2 = '<td><select name="car"><option value="volvo">Volvo</option><option value="saab">Saab</option></select></td>';
+                 var rowNode = table.row.add( [e1, e2, e3, e4, e5, e6] ).draw().node();
+
+                $( rowNode )
+                .css( 'color', 'red' )
+                .animate( { color: 'blue' } );
+                   console.log( table.data() );
+
+                 return false;
+             } );
+
+             $('#editBtn').click( function() {
+                  var rowNode = table.row('.selected');
+                  console.log(rowNode.data());
+                  var nodeArr =  rowNode.data();
+                  //for (i=0;i<nodeArr.length;i++){
+                 //  rowNode.remove().draw();
+                   table.row('.selected').data(["3",'<td><input type="text" name="fname" value='+nodeArr[0]+'></td>', nodeArr[1],"e","e2","e3","e4"] ).draw().node();
+                  //}
+                 return false;
+             } );
+
+             $('#delBtn').click( function() {
+                console.log(table.row($(this)));
+                var rowNode = table.row('.selected').remove().draw();
+                console.log( table.data() );
+                 return false;
+             } ); */
+         } ); 
 
 
 };
