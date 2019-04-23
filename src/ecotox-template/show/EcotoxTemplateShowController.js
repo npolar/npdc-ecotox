3ed0d8a646643da6c26126618613a95ada37745f
srldl <siri.uldal@gmail.com>
2019-04-23 16:22:59 +0200

    template modification

--- src/ecotox-template/show/EcotoxTemplateShowController.js
+++ src/ecotox-template/show/EcotoxTemplateShowController.js
@@ -11,8 +11,6 @@ var EcotoxTemplateShowController = function($controller, $routeParams,
   $scope.resource = EcotoxTemplate;
 
 
-
-
   //Show map in Antarctica
   $scope.mapOptions = {};
   $scope.mapOptions.color = "#FF0000";
@@ -23,11 +21,9 @@ var EcotoxTemplateShowController = function($controller, $routeParams,
     $scope.show().$promise.then((EcotoxTemplate) => {
 
       //Overlay the map with lat,lng
+      console.log($scope.document);
       $scope.mapOptions.coverage = [[[EcotoxTemplate.latitude,EcotoxTemplate.longitude],[EcotoxTemplate.latitude,EcotoxTemplate.longitude]]];
       $scope.mapOptions.geojson = "geojson";
-
-      $scope.document.lithology =  convert($scope.document.lithology);
-
     });
 
   };
@@ -35,20 +31,5 @@ var EcotoxTemplateShowController = function($controller, $routeParams,
   show();
 
 };
-
-/* convert from camelCase to lower case text*/
-function convert(str) {
-       var  positions = '';
-
-       for(var i=0; i<(str).length; i++){
-           if(str[i].match(/[A-Z]/) !== null){
-             positions += " ";
-             positions += str[i].toLowerCase();
-        } else {
-            positions += str[i];
-        }
-      }
-        return positions;
-}
 
 module.exports = EcotoxTemplateShowController;
