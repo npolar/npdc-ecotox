9f5db41519dfc32eb0d743373f47500ba5f704e8
srldl <siri.uldal@gmail.com>
2019-04-25 14:44:20 +0200

    test package

--- src/ecotox-fieldwork/edit/EcotoxFieldworkEditController.js
+++ src/ecotox-fieldwork/edit/EcotoxFieldworkEditController.js
@@ -5,15 +5,18 @@ var EcotoxFieldworkEditController = function($http, $scope, $location, $controll
   NpolarApiSecurity, npolarCountryService, NpdcSearchService, NpolarMessage) {
   'ngInject';
 
-
   var tb = require( '@srldl/edit-tabletest/js/edit-table.js');
+
 
   $controller('NpolarEditController', { $scope: $scope });
 
   $scope.resource = EcotoxTemplate;
-  $scope.ecotoxTab = tb.tabRow;
+
   tb.printMsg();
-  $scope.testMsg = tb.testMsg();
+  let obj = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+  $scope.testMsg = tb.testMsg(obj);
+
+  tb.testComponent();
 };
 
 
