ee31d758e15dda6ab1771b41af6ae9b2183c21f2
srldl <siri.uldal@gmail.com>
2019-08-27 13:43:32 +0200

    bumped up common

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var npdcGulp = require('npdc-gulp');
 var config = npdcGulp.baseConfig;
-config.COMMON_VERSION = '4.11.2';
+config.COMMON_VERSION = '4.11.3';
 npdcGulp.loadAppTasks(gulp, {
    'deps':{
      'assets': [
